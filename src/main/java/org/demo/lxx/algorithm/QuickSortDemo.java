4f4d51dadd35f451b328ece4a66e9ae378529a18
gitusersybchina <=>
2020-03-26 17:37:45 +0800

    [ADD]:前后指针交换法、栈非递归方式实现快排;

--- src/main/java/org/demo/lxx/algorithm/QuickSortDemo.java
+++ src/main/java/org/demo/lxx/algorithm/QuickSortDemo.java
@@ -3,6 +3,7 @@ package org.demo.lxx.algorithm;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * 快速排序练习
@@ -17,6 +18,38 @@ import java.util.Scanner;
  * @date : 2020/3/25 15:05
  */
 public class QuickSortDemo {
+
+
+    /**
+     * 非递归的方式，采用栈来实现排序
+     * 浪费空间换取时间
+     *
+     * @param array      待排序数组
+     * @param leftIndex  自左下标位置
+     * @param rightIndex 自右下标位置
+     */
+    public static void quickSortNotRecursive(int[] array, int leftIndex, int rightIndex) {
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(leftIndex);
+        stack.push(rightIndex);
+        // 栈不为空
+        while (!stack.empty()) {
+            int right = stack.pop();
+            int left = stack.pop();
+            int baseIndex = partQuickSortByPit(array, left, right);
+            // 左子序列
+            if ((baseIndex - 1) > left) {
+                stack.push(left);
+                stack.push(baseIndex - 1);
+            }
+            // 右子序列
+            if ((baseIndex + 1) < right) {
+                stack.push(baseIndex + 1);
+                stack.push(right);
+            }
+        }
+    }
 
 
     /**
@@ -54,9 +87,30 @@ public class QuickSortDemo {
             int baseIndex = partQuickSortByPointSwap(array, leftIndex, rightIndex);
             // ------------------ 递归部分(分治) -------------------------
             // 递归处理基准数左边的
-            quickSortByPit(array, leftIndex, baseIndex - 1);
+            quickSortByPointSwap(array, leftIndex, baseIndex - 1);
             // 递归处理基准数右边的
-            quickSortByPit(array, baseIndex + 1, rightIndex);
+            quickSortByPointSwap(array, baseIndex + 1, rightIndex);
+        }
+
+    }
+
+    /**
+     * 递归使用前后指针交换法
+     *
+     * @param array      待排序数组
+     * @param leftIndex  自左下标位置
+     * @param rightIndex 自右下标位置
+     */
+    public static void quickSortByPrePointSwap(int[] array, int leftIndex, int rightIndex) {
+
+        // 递归终止条件
+        if (leftIndex < rightIndex) {
+            int baseIndex = partQuickSortByPrePointSwap(array, leftIndex, rightIndex);
+            // ------------------ 递归部分(分治) -------------------------
+            // 递归处理基准数左边的
+            quickSortByPrePointSwap(array, leftIndex, baseIndex - 1);
+            // 递归处理基准数右边的
+            quickSortByPrePointSwap(array, baseIndex + 1, rightIndex);
         }
 
     }
@@ -79,7 +133,9 @@ public class QuickSortDemo {
     private static int partQuickSortByPit(int[] array, int leftIndex, int rightIndex) {
         // ------------------ 挖坑取数部分 -------------------------
         // 首次基准数字取了第一个元素
-        int baseNumber = array[leftIndex];
+//        int baseNumber = array[leftIndex];
+        // 优化后基准数字取数组首中尾三数的中间数字
+        int baseNumber = array[getBasIndexByMid(array, leftIndex, rightIndex)];
         while (leftIndex < rightIndex) {
             // 从右向左找第一个小于基准数的元素
             while (leftIndex < rightIndex && array[rightIndex] >= baseNumber) {
@@ -121,16 +177,18 @@ public class QuickSortDemo {
      */
     private static int partQuickSortByPointSwap(int[] array, int leftIndex, int rightIndex) {
         // 首次基准数字取了最后一个元素
-        int baseNumber = array[rightIndex];
+//        int baseNumber = array[rightIndex];
+        // 优化后基准数字取数组首中尾三数的中间数字
+        int baseNumber = array[getBasIndexByMid(array, leftIndex, rightIndex)];
         final int baseIndex = rightIndex;
         while (leftIndex < rightIndex) {
             // 从左向右找第一个小于等于基准数的元素
             while (leftIndex < rightIndex && array[leftIndex] <= baseNumber) {
-                ++leftIndex;
+                leftIndex++;
             }
             // 从右向左找第一个大于基准数的元素
             while (leftIndex < rightIndex && array[rightIndex] >= baseNumber) {
-                --rightIndex;
+                rightIndex--;
             }
             // 左右交换
             int temp = array[leftIndex];
@@ -144,6 +202,80 @@ public class QuickSortDemo {
         return leftIndex;
     }
 
+    /**
+     * 前后指针交换法
+     * 1. 定义变量cur指向序列的开头，定义变量pre指向cur的前一个位置。
+     * 2. 当array[cur] <  baseNumber 时，cur和pre同时往后走，如果array[cur]>baseNumber，cur往后走，pre留在大于key的数值前一个位置。
+     * 3. 当array[cur]再次 < baseNumber 时，交换array[cur]和array[pre]。
+     * 4. 当current走到终点时,交换pre++与right位置
+     * 通俗一点就是，在没找到大于baseNumber值前，pre永远紧跟cur，遇到大的两者之间就会拉开差距，中间差的肯定是连续的大于baseNumber的值，
+     * 当再次遇到小于baseNumber的值时，交换两个下标对应的值就好了。
+     *
+     * @param array      待排序数组
+     * @param leftIndex  左始下标
+     * @param rightIndex 右始下标
+     * @return 基准值新的位置
+     */
+    private static int partQuickSortByPrePointSwap(int[] array, int leftIndex, int rightIndex) {
+
+        if (leftIndex < rightIndex) {
+            // 首次基准数字取了最后一个元素
+//            int baseNumber = array[rightIndex];
+            // 优化后基准数字取数组首中尾三数的中间数字
+            int baseNumber = array[getBasIndexByMid(array, leftIndex, rightIndex)];
+            int currentIndex = leftIndex;
+            int preIndex = currentIndex - 1;
+            while (currentIndex < rightIndex) {
+                // 如果找到小于key的值，并且cur和pre之间有距离时则进行交换。注意两个条件的先后位置不能更换
+                while (array[currentIndex] < baseNumber && ++preIndex != currentIndex) {
+                    int temp = array[currentIndex];
+                    array[currentIndex] = array[preIndex];
+                    array[preIndex] = temp;
+                }
+                ++currentIndex;
+            }
+            // 此时current>=right,交换基准数字即right和pre+1的位置
+            int temp = array[rightIndex];
+            array[rightIndex] = array[++preIndex];
+            array[preIndex] = temp;
+            return preIndex;
+        }
+        return -1;
+    }
+
+    /**
+     * 三数取中法
+     * 上面的代码思想都是直接拿序列的最后一个值作为枢轴，
+     * 如果最后这个值刚好是整段序列最大或者最小的值，那么这次划分就是没意义的。
+     * 当序列是正序或者逆序时，每次选到的枢轴都是没有起到划分的作用。快排的效率会极速退化。
+     * 因此可以每次在选枢轴时，在序列的第一，中间，最后三个值里面选一个中间值出来作为枢轴，保证每次划分接近均等
+     *
+     * @param array      待排序数组
+     * @param leftIndex  左始下标
+     * @param rightIndex 右始下标
+     * @return 合理的基准数的下标
+     */
+    private static int getBasIndexByMid(int[] array, int leftIndex, int rightIndex) {
+
+        int midIndex = leftIndex + ((rightIndex - leftIndex) >> 1);
+        if (array[leftIndex] <= array[rightIndex]) {
+            if (array[midIndex] < array[leftIndex]) {
+                return leftIndex;
+            } else if (array[midIndex] > array[rightIndex]) {
+                return rightIndex;
+            } else {
+                return midIndex;
+            }
+        } else {
+            if (array[midIndex] < array[rightIndex]) {
+                return rightIndex;
+            } else if (array[midIndex] > array[leftIndex]) {
+                return leftIndex;
+            } else {
+                return midIndex;
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -153,19 +285,28 @@ public class QuickSortDemo {
             final int size = scanner.hasNext() ? Integer.parseInt(scanner.next()) : 0;
             final int[] array1 = new int[size];
             for (int i = 0; i < size; i++) {
-                array1[i] = random.nextInt(size);
+                array1[i] = random.nextInt(size * 10);
             }
             final int[] array2 = Arrays.copyOf(array1, array1.length);
-//            System.out.println("快速排序前输入参数为:" + Arrays.toString(array));
+            final int[] array3 = Arrays.copyOf(array1, array1.length);
+            final int[] array4 = Arrays.copyOf(array1, array1.length);
+            System.out.println("快速排序前输入参数为:" + Arrays.toString(array1));
             long beginTime = System.currentTimeMillis();
             quickSortByPit(array1, 0, size - 1);
-//            System.out.println("挖坑填数法快速排序输出结果为:" + Arrays.toString(array));
+//            System.out.println("挖坑填数法快速排序输出结果为:" + Arrays.toString(array1));
             System.out.println("挖坑填数法快速排序耗时为:" + (System.currentTimeMillis() - beginTime) + "毫秒");
-//            System.out.println("快速排序前输入参数为:" + Arrays.toString(array));
             beginTime = System.currentTimeMillis();
             quickSortByPointSwap(array2, 0, size - 1);
-//            System.out.println("左右指针交换法快速排序输出结果为:" + Arrays.toString(array));
+//            System.out.println("左右指针交换法快速排序输出结果为:" + Arrays.toString(array2));
             System.out.println("左右指针交换法快速排序耗时为:" + (System.currentTimeMillis() - beginTime) + "毫秒");
+            beginTime = System.currentTimeMillis();
+            quickSortByPrePointSwap(array3, 0, size - 1);
+//            System.out.println("前后指针交换法快速排序输出结果为:" + Arrays.toString(array3));
+            System.out.println("前后指针交换法快速排序耗时为:" + (System.currentTimeMillis() - beginTime) + "毫秒");
+            beginTime = System.currentTimeMillis();
+            quickSortNotRecursive(array4, 0, size - 1);
+            System.out.println("前后指针交换法快速排序输出结果为:" + Arrays.toString(array4));
+            System.out.println("非递归栈快速排序耗时为:" + (System.currentTimeMillis() - beginTime) + "毫秒");
         }
     }
 
